98280f3270b0ba95f77e97f606c56c5cdda7e004
premekrusek <premek@rusek.cz>
2026-04-19 21:02:42 +0200

    Added basic code for output

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,14 @@
 using namespace ftxui;
 
 int main() {
+    auto document = vbox({
+        text("Moje hra") | bold,
+        text("HP: 100") | color(Color::Green),
+    });
+
+    auto screen = Screen::Create(Dimension::Fit(document));
+    Render(screen, document);
+    screen.Print();
 
     return 0;
 }
