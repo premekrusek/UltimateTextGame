479255386b5303aace50c5909711f5455596f791
premekrusek <premek@rusek.cz>
2026-06-06 10:24:59 +0200

    Refactor UI callbacks into simple controller classes and clean up combat map rendering

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,75 @@
-#include <iostream>
-// #include <string>
-// #include <vector>
 #include <cstdlib>
-#include <ftxui/dom/elements.hpp>
-#include <ftxui/screen/screen.hpp>
-
+#include <ctime>
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/screen_interactive.hpp>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+#include <string>
+#include <vector>
 
 using namespace std;
 using namespace ftxui;
 
-int m = 100; // 16
-/*
-Min. jedenkrát musí hráč bojovat proti: 1 monstru v jednom setkání.
-Min. jedenkrát musí hráč bojovat proti: 2 monstrům v jednom setkání.
-Min. jedenkrát musí hráč bojovat proti: 3 monstrům v jednom setkání.
-Monstra mají 50% šanci mít u sebe peníze/zlato.
-*/
+int m = 100;  // 16
 int mB = 100; // 2
-/*
-Mini-boss má peníze/zlato vždy.
-*/
 int hB = 100; // 1
 int village = 2;
 int level = 1;
 
-// |V| > |M| > |M|> |2xM| > |MB| > |V| > |M| > |2xM| > |2xM| > |MB| > |V| >
-// |2xM| > |2xM| > |3xM| > |V| > |HB|
-
 enum class AppState { Menu, Village, Combat, Exit };
 AppState result = AppState::Menu;
+int selected_class = 1;
+
+struct Maps {
+  vector<string> combat_map = {
+      "+----------------------------+",
+      "|                            |",
+      "|                            |",
+      "|                            |",
+      "|                            |",
+      "|                            |",
+      "|                            |",
+      "|                            |",
+      "|                            |",
+      "|                            |",
+      "|                            |",
+      "|                            |",
+      "|                            |",
+      "|                            |",
+      "|                            |",
+      "+----------------------------+",
+  };
+
+  int height() { return combat_map.size(); }
+
+  int width() { return combat_map[0].size(); }
+
+  void setChar(int y, int x, char ch) {
+    if (y >= 0 && y < height() && x >= 0 && x < width()) {
+      combat_map[y][x] = ch;
+    }
+  }
+};
+
+struct Bullet {
+  int x;
+  int y;
+};
+
+struct Enemy {
+  int x = 20;
+  int y = 2;
+
+  void move(int dx) {
+    x += dx;
+    if (x < 2)
+      x = 2;
+    if (x > 27)
+      x = 27;
+  }
+};
 
 struct player {
-  // Hráč si na začátku hry zvolí classu. Hráč musí potvrdit, že chce dannou
-  // classu.
   string name;
   int maxHp;
   int hp;
@@ -47,19 +82,11 @@ struct player {
 
   int figure;
 
-  // Pozice hráče na obrazovce / mapě
   int x = 5;
   int y = 5;
 
-  // Vizuální podoba hráče
-  vector<string> sprite = {
-      " O ",
-      "/|\\", // Dvě zpětné lomítka, aby to rozpoznalo, že to je zpětné lomítko
-      "/ \\"};
-
   player(int f) {
     figure = f;
-
     switch (f) {
     case 1:
       name = "Paladin";
@@ -67,28 +94,24 @@ struct player {
       maxEnergy = energy = 100;
       attack_dmg = 3;
       break;
-
     case 2:
       name = "Lovec";
       maxHp = hp = 100;
       maxEnergy = energy = 100;
       attack_dmg = 4;
       break;
-
     case 3:
       name = "Mag";
       maxHp = hp = 100;
       maxEnergy = energy = 100;
       attack_dmg = 2;
       break;
-
     case 4:
       name = "Warlock";
       maxHp = hp = 100;
       maxEnergy = energy = 100;
       attack_dmg = 3;
       break;
-
     default:
       name = "Neznama classa";
       maxHp = hp = 100;
@@ -98,264 +121,46 @@ struct player {
     }
   }
 
-  // Pohyb hráče
-  void move(int dx, int dy) {
+  void move(int dx) {
     x += dx;
-    y += dy;
-
-    if (x < 0)
-      x = 0;
-
-    if (y < 0)
-      y = 0;
-
-    if (x > 37)
-      x = 37;
-
-    if (y > 17)
-      y = 17;
-  }
-
-  // Vykreslení hráče jako FTXUI Element
-  Element render() {
-    
-    vector<Element> rows;
-    
-    // vytvoření mapy
-    vector<string> map =
-    {
-      "+--------------------------------------+",
-      "|                                      |",
-      "|                                      |",
-      "|                                      |",
-      "|                                      |",
-      "|                                      |",
-      "|                                      |",
-      "|                                      |",
-      "|                                      |",
-      "|                                      |",
-      "|                                      |",
-      "|                                      |",
-      "|                                      |",
-      "|                                      |",
-      "|                                      |",
-      "|                                      |",
-      "|                                      |",
-      "|                                      |",
-      "|                                      |",
-      "|                                      |",
-      "|                                      |",
-      "|                                      |",
-      "|                                      |",
-      "|                                      |",
-      "+--------------------------------------+",
-    };
-
-    const int HEIGHT = map.size();
-    const int WIDTH = map[0].size();
-
-    // vykreslení hráče
-    map[y][x + 1] = 'O';
-
-    map[y + 1][x] = '/';
-    map[y + 1][x + 1] = '|';
-    map[y + 1][x + 2] = '\\';
-
-    map[y + 2][x] = '/';
-    map[y + 2][x + 2] = '\\';
-
-    // převod mapy na FTXUI elementy
-    for (int i = 0; i < HEIGHT; i++) {
-      rows.push_back(text(map[i]));
-    }
-
-    return vbox(std::move(rows)) | color(Color::Green);
-  }
-
-  void attack() {}
-
-  void ability1() {
-    switch (figure) {
-    case 1:
-      cout << "Paladin pouzil svaty uder!\n";
-      break;
-    case 2:
-      cout << "Lovec vystrelil ohnivy sip!\n";
-      break;
-    case 3:
-      cout << "Mag seslal fireball!\n";
-      break;
-    case 4:
-      cout << "Warlock seslal temnou kletbu!\n";
-      break;
-    }
-  }
-
-  void ability2() {
-    switch (figure) {
-    case 1:
-      cout << "Paladin se vylecil!\n";
-      break;
-    case 2:
-      cout << "Lovec vystrelil rychlou strelu!\n";
-      break;
-    case 3:
-      cout << "Mag se teleportoval!\n";
-      break;
-    case 4:
-      cout << "Warlock vysal zivot!\n";
-      break;
-    }
+    if (x < 1)
+      x = 1;
+    if (x > 26)
+      x = 26;
   }
 };
 
-void MainMenu(ScreenInteractive &screen) { // funkce pro celé menu
-  enum class MenuState { MainMenu, Characters, CharacterDetail, GameStart };
+class CombatController {
+public:
+  CombatController(ScreenInteractive &screen, player &p)
+      : screen(screen), p(p) {}
 
-  MenuState state = MenuState::MainMenu; // nastaví aktuální stav aplikace
-                                         // (začínáme v hlavním menu)
+  Element Render() {
+    Maps visible_map;
+    DrawPlayer(visible_map);
+    DrawEnemy(visible_map);
+    DrawBullets(visible_map);
 
-  // Hlavni menu
-  std::vector<std::string> main_entries = {
-      // seznam položek hlavního menu
-      "Start hry", // položka 0
-      "Postavy",   // položka 1
-      "Konec"      // položka 2
-  };
+    vector<Element> rows;
+    for (int i = 0; i < visible_map.height(); i++) {
+      rows.push_back(text(visible_map.combat_map[i]));
+    }
 
-  int main_selected = 0; // index aktuálně vybrané položky (defaultně první)
-  Component main_menu = Menu(
-      &main_entries,
-      &main_selected); // vytvoří interaktivní menu (napojené na data a výběr)
+    return vbox(rows) | color(Color::Green);
+  }
 
-  // Postavy
-  std::vector<std::string> characters = {// seznam postav
-                                         "Paladin", "Lovec", "Mag", "Warlock"};
+  bool OnEvent(Event event) {
+    MoveEnemy();
+    MoveBullets();
+    RemoveOldBullets();
 
-  int char_selected = 0; // index vybrané postavy
-  Component char_menu =
-      Menu(&characters, &char_selected); // menu pro výběr postavy
-
-  // Popisy postav
-  std::vector<std::string> descriptions = {
-      // textové popisy jednotlivých postav
-      "Paladin\nHP: 100\nEnergy: 100\nDMG: 3\n\nSchopnosti:\n- Svaty uder\n- "
-      "Leceni",
-
-      "Lovec\nHP: 100\nEnergy: 100\nDMG: 4\n\nSchopnosti:\n- Ohnivy sip\n- "
-      "Rychla strela",
-
-      "Mag\nHP: 100\nEnergy: 100\nDMG: 2\n\nSchopnosti:\n- Fireball\n- "
-      "Teleport",
-
-      "Warlock\nHP: 100\nEnergy: 100\nDMG: 3\n\nSchopnosti:\n- (zatim "
-      "nedefinovano)\n- (zatim nedefinovano)"};
-
-  // Rendrovani
-  Component container = Container::Vertical({
-      // kontejner, který drží komponenty (menu)
-      main_menu, // hlavní menu
-      char_menu  // menu postav
-  });
-
-  Component renderer =
-      Renderer(container, [&] { // renderer říká jak vykreslit UI podle stavu
-        if (state == MenuState::MainMenu) { // pokud jsme v hlavním menu
-          return vbox({
-                     // vykreslí vertikální sloupec
-                     text("=== HLAVNÍ MENU ===") | bold, // nadpis (tučný)
-                     main_menu->Render(),   // vykreslí samotné menu
-                     text("ENTER = vybrat") // nápověda
-                 }) |
-                 border;                             // přidá rámeček
-        } else if (state == MenuState::Characters) { // pokud jsme v menu postav
-          return vbox({text("=== POSTAVY ===") | bold,
-                       char_menu->Render(), // vykreslí menu postav
-                       text("ENTER = vybrat | ESC = zpět")}) |
-                 border;
-        } else if (state ==
-                   MenuState::CharacterDetail) { // pokud jsme v detailu postavy
-          return vbox({text("=== DETAIL POSTAVY ===") | bold,
-                       text(descriptions[char_selected]), // zobrazí popis
-                                                          // vybrané postavy
-                       text("ESC = zpět")}) |
-                 border;
-        } else if (state == MenuState::GameStart) { // pokud jsme ve hře
-          result = AppState::Combat;
-          return vbox({text("UltimateTextGame") | bold, text("")});
-        }
-
-        return text("Chyba v menu"); // fallback (když by byl neplatný stav)
-      });
-
-  // Eventy
-  Component component =
-      CatchEvent(renderer, [&](Event event) { // zachytává klávesy / vstupy
-        // Hlavni menu
-        if (state == MenuState::MainMenu) { // pokud jsme v hlavním menu
-          if (event == Event::Return) {     // pokud uživatel stiskne ENTER
-            if (main_selected == 0) {       // pokud je vybráno "Start hry"
-              state = MenuState::GameStart;
-              result = AppState::Combat; // Village
-              screen.Exit();
-              return true; // event byl zpracován
-            }
-            if (main_selected == 1) {        // pokud je vybráno "Postavy"
-              state = MenuState::Characters; // přepne do menu postav
-              return true;                   // event byl zpracován
-            }
-            if (main_selected == 2) { // pokud je vybráno "Konec"
-              result = AppState::Exit;
-              screen.Exit(); // ukončí aplikaci
-              return true;
-            }
-          }
-        }
-
-        // Postavy
-        if (state == MenuState::Characters) {   // pokud jsme v menu postav
-          if (event == Event::Return) {         // ENTER
-            state = MenuState::CharacterDetail; // otevře detail postavy
-            return true;
-          }
-          if (event == Event::Escape) {  // ESC
-            state = MenuState::MainMenu; // návrat do hlavního menu
-            return true;
-          }
-        }
-
-        // Detail postavy
-        if (state == MenuState::CharacterDetail) { // pokud jsme v detailu
-          if (event == Event::Escape) {            // ESC
-            state = MenuState::Characters;         // návrat do seznamu postav
-            return true;
-          }
-        }
-
-        return false; // event nebyl zpracován (pošle se dál)
-      });
-
-  screen.Loop(component); // spustí hlavní smyčku
-                          /*
-                          while (běží program) {
-                              načti vstup (klávesy)
-                              pošli ho do component (OnEvent)
-                              překresli obrazovku (Render)
-                          }*/
-}
-
-void Combat(ScreenInteractive &screen, player &p) {
-  Component empty = Container::Vertical({});
-  Component renderer = Renderer(empty, [&] { return p.render(); });
-
-  Component component = CatchEvent(renderer, [&](Event event) {
     if (event == Event::ArrowLeft) {
-      p.move(-2, 0);
+      p.move(-2);
       return true;
     }
 
     if (event == Event::ArrowRight) {
-      p.move(2, 0);
+      p.move(2);
       return true;
     }
 
@@ -366,16 +171,228 @@ void Combat(ScreenInteractive &screen, player &p) {
     }
 
     return false;
-  });
+  }
+
+private:
+  ScreenInteractive &screen;
+  player &p;
+  Enemy enemy;
+  vector<Bullet> bullets;
+
+  void DrawPlayer(Maps &map) {
+    map.setChar(p.y, p.x + 1, 'O');
+    map.setChar(p.y + 1, p.x, '/');
+    map.setChar(p.y + 1, p.x + 1, '|');
+    map.setChar(p.y + 1, p.x + 2, '\\');
+    map.setChar(p.y + 2, p.x, '/');
+    map.setChar(p.y + 2, p.x + 2, '\\');
+  }
+
+  void DrawEnemy(Maps &map) {
+    map.setChar(enemy.y, enemy.x, 'O');
+    map.setChar(enemy.y + 1, enemy.x, '|');
+    map.setChar(enemy.y + 2, enemy.x - 1, '/');
+    map.setChar(enemy.y + 2, enemy.x + 1, '\\');
+  }
+
+  void DrawBullets(Maps &map) {
+    for (int i = 0; i < bullets.size(); i++) {
+      map.setChar(bullets[i].y, bullets[i].x, 'I');
+    }
+  }
+
+  void MoveEnemy() {
+    if (rand() % 2)
+      enemy.move(2);
+    else
+      enemy.move(-2);
+  }
+
+  void MoveBullets() {
+    if (rand() % 5 == 0) {
+      Bullet bullet;
+      bullet.x = enemy.x;
+      bullet.y = enemy.y + 3;
+      bullets.push_back(bullet);
+    }
+
+    for (int i = 0; i < bullets.size(); i++) {
+      bullets[i].y++;
+    }
+  }
+
+  void RemoveOldBullets() {
+    Maps map;
+    vector<Bullet> active_bullets;
+
+    for (int i = 0; i < bullets.size(); i++) {
+      if (bullets[i].y < map.height()) {
+        active_bullets.push_back(bullets[i]);
+      }
+    }
+
+    bullets = active_bullets;
+  }
+};
+
+class MainMenuController {
+public:
+  MainMenuController(ScreenInteractive &screen) : screen(screen) {
+    main_menu = Menu(&main_entries, &main_selected);
+    char_menu = Menu(&characters, &char_selected);
+    container = Container::Vertical({main_menu, char_menu});
+  }
+
+  Component GetContainer() { return container; }
+
+  Element Render() {
+    if (state == MenuState::MainMenu) {
+      return vbox({text("=== HLAVNÍ MENU ===") | bold, main_menu->Render(),
+                   text("ENTER = vybrat")}) |
+             border;
+    }
+
+    if (state == MenuState::Characters) {
+      return vbox({text("=== POSTAVY ===") | bold, char_menu->Render(),
+                   text("ENTER = vybrat | ESC = zpět")}) |
+             border;
+    }
+
+    if (state == MenuState::CharacterDetail) {
+      return vbox({text("=== DETAIL POSTAVY ===") | bold,
+                   text(descriptions[char_selected]),
+                   text("ESC = zpět | ENTER = vybrat tuto postavu")}) |
+             border;
+    }
+
+    return text("Chyba v menu");
+  }
+
+  bool OnEvent(Event event) {
+    if (state == MenuState::MainMenu) {
+      return HandleMainMenu(event);
+    }
+
+    if (state == MenuState::Characters) {
+      return HandleCharacters(event);
+    }
+
+    if (state == MenuState::CharacterDetail) {
+      return HandleCharacterDetail(event);
+    }
+
+    return false;
+  }
+
+private:
+  enum class MenuState { MainMenu, Characters, CharacterDetail };
+
+  ScreenInteractive &screen;
+  MenuState state = MenuState::MainMenu;
+
+  vector<string> main_entries = {"Start hry", "Postavy", "Konec"};
+  int main_selected = 0;
+  Component main_menu;
+
+  vector<string> characters = {"Paladin", "Lovec", "Mag", "Warlock"};
+  int char_selected = 0;
+  Component char_menu;
+
+  vector<string> descriptions = {
+      "Paladin\nHP: 100\nEnergy: 100\nDMG: 3\n\nSchopnosti:\n- Svaty uder\n- "
+      "Leceni",
+      "Lovec\nHP: 100\nEnergy: 100\nDMG: 4\n\nSchopnosti:\n- Ohnivy sip\n- "
+      "Rychla strela",
+      "Mag\nHP: 100\nEnergy: 100\nDMG: 2\n\nSchopnosti:\n- Fireball\n- "
+      "Teleport",
+      "Warlock\nHP: 100\nEnergy: 100\nDMG: 3\n\nSchopnosti:\n- (zatim "
+      "nedefinovano)\n- (zatim nedefinovano)"};
+
+  Component container;
+
+  bool HandleMainMenu(Event event) {
+    if (event != Event::Return) {
+      return false;
+    }
+
+    if (main_selected == 0) {
+      result = AppState::Combat;
+      selected_class = char_selected + 1;
+      screen.Exit();
+      return true;
+    }
+
+    if (main_selected == 1) {
+      state = MenuState::Characters;
+      return true;
+    }
+
+    if (main_selected == 2) {
+      result = AppState::Exit;
+      screen.Exit();
+      return true;
+    }
+
+    return false;
+  }
+
+  bool HandleCharacters(Event event) {
+    if (event == Event::Return) {
+      state = MenuState::CharacterDetail;
+      return true;
+    }
+
+    if (event == Event::Escape) {
+      state = MenuState::MainMenu;
+      return true;
+    }
+
+    return false;
+  }
+
+  bool HandleCharacterDetail(Event event) {
+    if (event == Event::Return) {
+      selected_class = char_selected + 1;
+      state = MenuState::MainMenu;
+      return true;
+    }
+
+    if (event == Event::Escape) {
+      state = MenuState::Characters;
+      return true;
+    }
+
+    return false;
+  }
+};
+
+void MainMenu(ScreenInteractive &screen) {
+  MainMenuController menu(screen);
+
+  Component renderer =
+      Renderer(menu.GetContainer(), [&] { return menu.Render(); });
+
+  Component component =
+      CatchEvent(renderer, [&](Event event) { return menu.OnEvent(event); });
+
+  screen.Loop(component);
+}
+
+void Combat(ScreenInteractive &screen, player &p) {
+  CombatController combat(screen, p);
+
+  Component empty = Container::Vertical({});
+  Component renderer = Renderer(empty, [&] { return combat.Render(); });
+
+  Component component =
+      CatchEvent(renderer, [&](Event event) { return combat.OnEvent(event); });
 
   screen.Loop(component);
 }
 
 void Village(ScreenInteractive &screen, player &p) {
   Component empty = Container::Vertical({});
-
   Component renderer = Renderer(empty, [&] { return text("Village"); });
-
   Component component = CatchEvent(renderer, [&](Event event) {
     if (event == Event::Escape) {
       result = AppState::Menu;
@@ -389,7 +406,7 @@ void Village(ScreenInteractive &screen, player &p) {
 
 int main() {
   ScreenInteractive screen = ScreenInteractive::TerminalOutput();
-  player Player(1);
+  srand(time(nullptr));
 
   while (result != AppState::Exit) {
     switch (result) {
@@ -397,17 +414,18 @@ int main() {
       system("clear");
       MainMenu(screen);
       break;
-
-    case AppState::Combat:
+    case AppState::Combat: {
       system("clear");
+      player Player(selected_class);
       Combat(screen, Player);
       break;
-
-    case AppState::Village:
+    }
+    case AppState::Village: {
       system("clear");
+      player Player(selected_class);
       Village(screen, Player);
       break;
-
+    }
     default:
       break;
     }
